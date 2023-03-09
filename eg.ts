43816a06f9a1cf474acbea4bad6283139b9b4d45
Your Name <you@example.com>
2023-03-09 22:34:37 +0530

    Practice code

--- eg.ts
+++
@@ -1,4 +0,0 @@
-var ds:any={name:"Vivek",age:"20"};
-    var dataSchema:any=JSON.stringify(ds);
-    console.log(dataSchema);
-    
---
+++ public/javascripts/index.js
@@ -0,0 +1,42 @@
+
+
+// const dotenv=require('dotenv');
+// const Logger = require('nodemon/lib/utils/log');
+// dotenv.config();
+// var pg = require("pg");
+// var {Client}=require("pg");
+
+// // var conString = "postgres://user1:JER@ALD@localhost:5432/student";
+// // var client = new pg.Client(conString);
+
+
+
+// const connectDb = async () => {
+//     // try {
+//             const pool = new Client({
+//                 user: 'user1',
+//                 host: 'localhost',
+//                 database: 'student',
+//                 password: 'JER@ALD',
+//                 port: 5432,
+//         })
+
+//         await pool.connect()
+//         const res = await pool.query("INSERT INTO students(name, phno, address) VALUES('John', 5, 'Bngalore')");
+
+//         console.log(res)
+// //         client.connect();
+// //         var x = 1000;
+
+// // while (x > 0) {
+// //     // client.query("INSERT INTO junk(name, a_number) values('Ted',12)");
+// //     client.query("INSERT INTO students(name, phno, address) values($1, $2, $3)", ['John', 5, 'Bngalore']);
+// //     x = x - 1;
+// // }
+//         await pool.end()
+//     // } catch (error) {
+//     //     console.log("cannot connect");
+
+//     // }
+// }
+// connectDb()

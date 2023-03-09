03a4221bcf0d3591fb46bfbb4f478090183193a5
Your Name <you@example.com>
2023-03-09 11:24:07 +0530

    code updated

--- dataSets.ts
+++ dataSets.ts
@@ -1,4 +1,5 @@
 import { error } from "console";
+import { SrvRecord } from "dns";
 
 const express = require("express");
 const app = express();
@@ -6,6 +7,8 @@ const port = 3001;
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const { Client } = require("pg");
+
+
 // var getDataSet=require("./getDataSet.ts");
 
 // app.use(bodyParser.urlencoded({extended:true}));
@@ -22,73 +25,86 @@ app.get("/dataset/get", (req: any, res: any) => {
         port: 5432,
       });
       await client.connect();
-      
-      const gotData = await client.query("SELECT * FROM datasets");
-      if(gotData.rowCount>0){
-        res.send(gotData);
-      }
-      else{
-        var detail: string = `Table is empty`;
-          var errorStatus: string = "ERROR";
-          const obj1 = {
-            status: `${errorStatus}`,
-            message: `${detail}`
-          };
-          const statusJson = JSON.stringify(obj1);
 
-          
-          res.send(statusJson);
+      const gotData = await client.query("SELECT * FROM datasets");
+      if (gotData.rowCount > 0) {
+        res.send(gotData);
+      } else {
+        var detail: string = `Table is empty`;
+        var errorStatus: string = "ERROR";
+        const obj1 = {
+          status: `${errorStatus}`,
+          message: `${detail}`,
+        };
+        const statusJson = JSON.stringify(obj1);
+
+        res.send(statusJson);
       }
       await client.end();
     } catch (error) {
-        console.log(error);
-        
+      console.log(error);
     }
   };
   connectDb();
 });
 
 app.get("/dataset/getById", (req: any, res: any) => {
-  const connectDb = async () => {
-    try {
-      const client = new Client({
-        user: "user1",
-        host: "localhost",
-        database: "datasets",
-        password: "JER@ALD",
-        port: 5432,
-      });
-      await client.connect();
-      var getid=req.query.id;
-      var id=Number(getid);
-    
-        const gotData = await client.query(`SELECT * FROM datasets WHERE id='${id}'`);
+  interface dataObject{
+    id:string;
+    data_schema:Object;
+    router_config:Object;
+    status:string;
+    created_by:string;
+    updated_by:string;
+    created_date:Date;
+    updated_date:Date;
+  }
+  const id:dataObject=req.body.id;
+  console.log(id);
+  
+  res.send("helo");
+  // const connectDb = async () => {
+  //   try {
+  //     const client = new Client({
+  //       user: "user1",
+  //       host: "localhost",
+  //       database: "datasets",
+  //       password: "JER@ALD",
+  //       port: 5432,
+  //     });
+  //     await client.connect();
+  //     var getid = req.query.id;
+  //     var id = Number(getid);
 
-        if(gotData.rowCount>0)
-        res.send(gotData);
-        else{
-          var detail: string = `Key (id)=(${id}) does not exist.`;
-          var errorStatus: string = "ERROR";
-          const obj1 = {
-            status: `${errorStatus}`,
-            message: `${detail}`
-          };
-          const statusJson = JSON.stringify(obj1);
-          res.send(statusJson);
-        }
-      await client.end();
-     return true;
-    } catch (error) {
-      console.log(error);
-      
-    }
-  };
-  connectDb();
+  //     const gotData = await client.query(
+  //       `SELECT * FROM datasets WHERE id='${id}'`
+  //     );
+
+  //     if (gotData.rowCount > 0) res.send(gotData);
+  //     else {
+  //       var detail: string = `Key (id)=(${id}) does not exist.`;
+  //       var errorStatus: string = "ERROR";
+  //       const obj1 = {
+  //         status: `${errorStatus}`,
+  //         message: `${detail}`,
+  //       };
+  //       const statusJson = JSON.stringify(obj1);
+  //       res.send(statusJson);
+  //     }
+  //     await client.end();
+  //     return true;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // connectDb();
 });
 
 app.post("/dataset/create", (req: any, res: any) => {
   
-
+     
+      
+      
   const connectDb = async () => {
     let pK: any;
     try {
@@ -102,14 +118,15 @@ app.post("/dataset/create", (req: any, res: any) => {
 
       // console.log(req.body.dname);
 
-      
       var id = req.body.id;
       var name1 = req.body.dname;
       var age = req.body.age;
       var name2 = req.body.rname;
       var method = req.body.method;
-      var ds = { name: `${name1}`, age: `${age}` };
-      var rc = { name: `${name2}`, method: `${method}` };
+      var ds = req.body.data_schema;
+      // { name: `${name1}`, age: `${age}` };
+      var rc = req.body.router_config;
+      // { name: `${name2}`, method: `${method}` };
       var dataSchema = JSON.stringify(ds);
       var routerConfig = JSON.stringify(rc);
       var status1: string = req.body.status;
@@ -122,82 +139,51 @@ app.post("/dataset/create", (req: any, res: any) => {
 
       await client.connect();
       const pk = await client.query(`SELECT * FROM datasets WHERE '${id}'=id`);
-      const Nid=Number(id);
-
-    
-      
-
+      const Nid = Number(id);
 
       if (pk.rowCount == 0) {
-        if(Nid){
+        if (Nid) {
           await client.query(
             `INSERT INTO datasets(id, data_schema, router_config, status, created_by, updated_by, created_date) VALUES('${id}', '${dataSchema}', '${routerConfig}', '${status1}', '${createdBy}', '${updatedBy}', '${createdDate}');`
           );
-          
-          
-  
           fs.readFile(
             __dirname + "/success.json",
             "utf8",
             function (err: any, data: any) {
-              var message=JSON.parse(data);
-              
+              var message = JSON.parse(data);
+
               res.send(message);
             }
           );
-        }
-        else{
-        var detail: string = `JSON Input data missing`;
-        var errorStatus: string = 'ERROR';
-        const obj1:{status:string,message:string}= {
+        } else {
+          var detail: string = `JSON Input data missing`;
+          var errorStatus: string = "ERROR";
+          const obj1: { status: string; message: string } = {
             status: `${errorStatus}`,
-          message: `${detail}`
-        };
-        const statusJson = JSON.stringify(obj1);
-        // const STST:JSON=JSON.parse(statusJson);
-
-        // console.log(typeof STST);
-        // console.log(typeof statusJson);
-        // console.log(statusJson);
-        
-        
-        
-        res.send(statusJson);
+            message: `${detail}`,
+          };
+          const statusJson = JSON.stringify(obj1);
+          res.send(statusJson);
         }
-        
-        
       } else {
-        var errorCode: string = "23505";
+        // var errorCode: string = "400";
         var detail: string = `Key (id)=(${id}) already exists.`;
         var errorStatus: string = "ERROR";
         const obj1 = {
           status: `${errorStatus}`,
-          message: `${detail}`,
-          error_code: `${errorCode}`,
+          message: `${detail}`
+          // error_code: `${errorCode}`,
         };
         const statusJson = JSON.stringify(obj1);
         console.log(statusJson);
-        
+
         res.send(statusJson);
       }
-
-    
 
       await client.end();
       return true;
     } catch (error: any) {
-   
-
       console.log(error);
-
-      // if(pK.rowCount==1){
-      //   var errorCode:string=error.code;
-      // var detail:string=error.detail;
-      // var errorStatus:string=error.severity;
-      // const obj1=`{status:${errorStatus},message:${detail},error_code:${errorCode}}`;
-      // const statusJson=JSON.parse(obj1);
-      // res.send(statusJson);
-      // }
     }
   };
   connectDb();
@@ -221,6 +207,7 @@ app.put("/datasets/updateData", (req: any, res: any) => {
       var rmethod = req.body.method1;
       var rc = { name: `${rname}`, method: `${rmethod}` };
       var routerConfig = JSON.stringify(rc);
+
       // var status=req.body.status;
       // var updatedDate:any=new Date().getFullYear()+"-"+((new Date().getMonth())+1)+"-"+new Date().getHours()+" "+new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds();
       // var createdDate:any=new Date().getFullYear()+"-"+((new Date().getMonth())+1)+"-"+new Date().getHours()+" "+new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds();;

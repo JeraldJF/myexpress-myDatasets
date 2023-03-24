bbcb5d86bd589c6ae2e07741ea5643069c23b990
Your Name <you@example.com>
2023-03-24 18:16:19 +0530

    created env

--- dist/Config/Connection.js
+++ dist/Config/Connection.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { Pool } = require('pg');
 var pool = new Pool({
     user: "user1",
-    host: "localhost",
-    database: "datasets",
-    password: "JER@ALD",
+    host: "localhost" || process.env.HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
     port: 5432,
 });
 pool.connect();
